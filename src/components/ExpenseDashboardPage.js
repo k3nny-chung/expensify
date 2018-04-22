0507baeb789b222b2448c43304e81088bd87748f
kenny <k3nny.chung@gmail.com>
2018-04-22 14:56:59 -0400

    Add expense summary to dashboard

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilter from './ExpenseListFilter';
+import ExpenseSummary from './ExpenseSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
         <h1>Dashboard</h1>
         <ExpenseListFilter />
+        <ExpenseSummary />
         <ExpenseList />
     </div>
 );
